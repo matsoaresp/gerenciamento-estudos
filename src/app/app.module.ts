22a6d409d118a58da5033278807e0f487298a920
Mateus Prado <maateussp@gmail.com>
2026-03-12 08:49:34 -0300

    feat: typeorm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,22 @@ import { AppController } from './app.controller';
 
 import { UserModule } from '../user/user.module';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5433,
+      username: 'postgres',
+      database: 'gerencia-materias',
+      password: '123456',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
