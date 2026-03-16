671006ddffcb788199f262fe759ec1e7ba5c861c
Mateus Prado <maateussp@gmail.com>
2026-03-16 11:52:18 -0300

    fix: corrigindo injectRepository

--- src/progresso/progresso.module.ts
+++ src/progresso/progresso.module.ts
@@ -3,17 +3,21 @@ import { ProgressoService } from './progresso.service';
 import { ProgressoController } from './progresso.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Progresso } from './entities/progresso.entity';
-import { TopicosEnum } from 'src/topicos/enum/topicos.enum';
 import { TopicosModule } from 'src/topicos/topicos.module';
 import { Topico } from 'src/topicos/entities/topico.entity';
+import { Metas } from 'src/metas/entities/meta.entity';
 
 @Module({
   controllers: [ProgressoController],
   providers: [ProgressoService],
   imports: [
-    TypeOrmModule.forFeature([Progresso, Topico]),
+    TypeOrmModule.forFeature([
+      Progresso,
+      Topico,
+      Metas
+    ]),
     TopicosModule,
   ],
-  exports: [ProgressoModule]
+  exports: [ProgressoService]
 })
 export class ProgressoModule {}
