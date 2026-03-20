5084bb04211cfa27dbba5cbfbb82f9a80fbb9b8d
Mateus Prado <maateussp@gmail.com>
2026-03-20 08:50:04 -0300

    feat: codigo do modulo

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,39 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/user/entities/user.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import jwtConfig from './config/jwt.config';
+import { JwtModule } from '@nestjs/jwt';
 
+@Global()
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    ConfigModule.forFeature(jwtConfig),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (ConfigService: ConfigService) => {
+        const jwt = ConfigService.get('jwt');
+
+        return {
+          secret: jwt.secret,
+          signOptions: {
+            audience: jwt.audience,
+            issuer: jwt.issuer,
+            expiresIn: jwt.jwtTtl,
+          },
+        };
+      },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    AuthService
+  ],
+
+  exports: [JwtModule, ConfigModule]
 })
 export class AuthModule {}
